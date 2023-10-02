ab7e7af5ee2fad908c2050b29bc678837be954c6
Adarsh Unni <adarshunni.work@gmail.com>
2023-10-03 01:08:42 +0530

    Stop game after winner announced

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
 function player(name) {
     this.name = name
     this.weapon = (marker) => {
-        this.marker = marker 
-    } 
+        this.marker = marker
+    }
+ 
 }
-
-const playerOne = new player("Player 1")
-const playerTwo = new player("Player 2")
-
-playerOne.weapon('X')
-playerTwo.weapon('O')
 
 function gameBoard(arr,input,playerNumber) {
     if (playerNumber === playerOne){
@@ -20,65 +15,89 @@ function gameBoard(arr,input,playerNumber) {
     }
     console.log(arr)
 }
-let winner = "no"
 
-function game(arr, winner){
+function game(arr,Winner){
     for (let i=0; i<5; i++){
         if (i === 0 ){
             if (((arr[i] === arr[i+1] && arr[i+1] === arr[i+2]) || (arr[i] === arr[i+3] && arr[i+3] === arr[i+6]) || (arr[i] === arr[i+4] && arr[i+4] === arr[i+8])) && (arr[i] !== "none")){
-                announcementContent.textContent = "WINNER!!!" 
-                winner = "yes"
-            }
+                announcementContent.textContent = "WINNER!!!"     
+                Winner = "found"
+                gameFinished = "yes"
+            }       
         }
         else if (i === 1){
             if ((arr[i] === arr[i+3] && arr[i+3] === arr[i+6]) && arr[i] !== "none"){
                 announcementContent.textContent = "WINNER!!!"
-                winner = "yes"
+                Winner = "found"
             }
         }
         else if (i === 2){
             if (((arr[i] === arr[i+2] && arr[i+2] === arr[i+4]) || (arr[i] === arr[i+3] && arr[i+3] === arr[i+6])) && arr[i] !== "none"){
                 announcementContent.textContent = "WINNER!!!"
-                winner = "yes"
+                Winner = "found"
             } 
         }
         else if (i === 3){
             if ((arr[i] === arr[i+1] && arr[i+1] === arr[i+2]) && arr[i] !== "none"){
                 announcementContent.textContent = "WINNER!!!"
-                winner = "yes"
+                Winner = "found"
             }
         }
         else{
             if ((arr[i+2] === arr[i+3] && arr[i+3] === arr[i+4]) && arr[i+2] !== "none"){
                 announcementContent.textContent = "WINNER!!!"
-                winner = "yes"
+                Winner = "found"
             }
         }
     }
+    let found = "false"
+    for (let i=0; i<arr.length; i++){
+        if (arr[i] === "none"){
+            found = "true"
+            break
+        }
+    }
+    if (found === "false" && Winner === "notFound"){
+        announcementContent.textContent = "It's a TIE !!!"
+    }
+    
 }
+    
+
+const playerOne = new player("Player 1")
+const playerTwo = new player("Player 2")
 
 const gridItems = document.querySelectorAll('.grid-item')
-
-let currentPlayer = playerOne
 
 const newGame = document.querySelectorAll('button')
 
 const announcement = document.querySelector('.announcement')
 const announcementContent = document.createElement('div')
 announcement.appendChild(announcementContent)
+announcementContent.textContent = "Click play to start !!!"
 announcementContent.style.fontSize = '32px';
 announcementContent.style.fontFamily = 'Times New Roman'
 
+let gameFinished = "no"
 for (let i=0; i<newGame.length; i++){
     newGame[i].addEventListener('click', () => {
         let arr = []
         for (let i=0; i<9; i++){
             arr[i] = "none"
+            gridItems[i].textContent = null
         }
-        announcementContent.textContent = "New Game!!! Next Turn: Player 1"
+
+        let currentPlayer = playerOne
+        playerOne.weapon('X')
+        playerTwo.weapon('O')
+        let Winner = "notFound"
+        announcementContent.textContent = "New Game!!! Player 1, it's your turn."
+
+        gameFinished = "no"
+
         for (let i=0; i<gridItems.length ; i++){
             gridItems[i].addEventListener('click',() => {
-                if (arr[i] === "none" && winner === "no"){
+                if (arr[i] === "none" && gameFinished === "no"){
                     if (currentPlayer === playerOne){
                         gameBoard(arr,i,playerOne)
                         announcementContent.textContent = "Next Turn: Player 2"
@@ -87,18 +106,20 @@ for (let i=0; i<newGame.length; i++){
                         gameBoard(arr,i,playerTwo)
                         announcementContent.textContent = "Next Turn: Player 1"
                     }
+
                     if (currentPlayer === playerOne){
                         currentPlayer = playerTwo
                     }
                     else{
                         currentPlayer = playerOne
                     }
-                    game(arr, winner)
+                    game(arr,Winner)
                 }
             })
         }
     })
 }
+
 
 
 
